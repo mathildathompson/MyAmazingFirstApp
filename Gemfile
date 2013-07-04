026d657e7fd2237e40433df73d4920293be52a3f
Mathilda Thompson <mathildathompson288@gmail.com>
2013-07-04 14:36:18 +0100

    Changed the content

--- Gemfile
+++ Gemfile
@@ -2,4 +2,6 @@ source 'https://rubygems.org'
  
 gem 'sinatra'
 gem 'twilio-ruby'
-gem 'rack-flash3'
+# gem 'rack-flash3'
+# gem "sinatra-flash", "~> 0.3.0"
+gem 'pry'
--- my_amazing_web_app.rb
+++ my_amazing_web_app.rb
@@ -1,18 +1,43 @@
 require 'twilio-ruby'
 require 'sinatra'
-require 'rack-flash'
+# require 'slim'
+require 'sinatra/flash'
+require 'pry'
 
 enable :sessions
 set :session_secret, 'super secret'
-use Rack::Flash
+# use Rack::Flash
 
 get '/' do
   erb :index
 end
-
+ 
 post '/submit' do
-  @message = "#{params[:full_name]} you chose #{params[:answer]} puppies"
+  # binding.pry
+  if params['answer1'] == '21 million' && params['answer2'] == '2008' && params['answer'] == "1 billion US dollars"
+  @message = "#{params[:full_name]} your answers were 1) #{params[:answer1]} 2) #{params[:answer2]} 3)#{params[:answer]} and you scored 100% "
+  # binding.pry
+  elsif params['answer1'] == '21 million' && params['answer2'] == '2008' && params['answer'] != "1 billion US dollars"
+  @message = "#{params[:full_name]} your answers were 1) #{params[:answer1]} 2) #{params[:answer2]} 3)#{params[:answer]} and you scored 66.6% "
+  # binding.pry
+  elsif params['answer1'] != '21 million' && params['answer2'] == '2008' && params['answer'] == "1 billion US dollars"
+  @message = "#{params[:full_name]} your answers were 1) #{params[:answer1]} 2) #{params[:answer2]} 3)#{params[:answer]} and you scored 66.6% "
+  # binding.pry
+  elsif params['answer1'] == '21 million' && params['answer2'] != '2008' && params['answer'] == "1 billion US dollars"
+  @message = "#{params[:full_name]} your answers were 1) #{params[:answer1]} 2) #{params[:answer2]} 3)#{params[:answer]} and you scored 66.6% "
+  # binding.pry
+  elsif params['answer1'] == '21 million' && params['answer2'] != '2008' && params['answer'] != "1 billion US dollars"
+  @message = "#{params[:full_name]} your answers were 1) #{params[:answer1]} 2) #{params[:answer2]} 3)#{params[:answer]} and you scored 33.3% "
+
+  elsif params['answer1'] != '21 million' && params['answer2'] != '2008' && params['answer'] == "1 billion US dollars"
+  @message = "#{params[:full_name]} your answers were 1) #{params[:answer1]} 2) #{params[:answer2]} 3)#{params[:answer]} and you scored 33.3% "
+
+  elsif params['answer1'] != '21 million' && params['answer2'] == '2008' && params['answer'] != "1 billion US dollars"
+  @message = "#{params[:full_name]} your answers were 1) #{params[:answer1]} 2) #{params[:answer2]} 3)#{params[:answer]} and you scored 33.3% "
   
+  else
+  @message = 'You scored 0, listen harder next time!'
+  end
   # Change these to match your Twilio account settings 
   @account_sid = "AC90ac63932da17a7495ae86ce14a10f8a"
   @auth_token = "f032751ec043326954be6d67b3b09a2d"
@@ -23,10 +48,10 @@ post '/submit' do
   @account = @client.account
   @sms = @account.sms.messages.create({
     :from => '+441173251793', 
-    :to => '+447867986492',
+    :to => params['phone_number'],
     :body => @message
   })
   
-  flash[:notice] = "SMS sent: #{@message}"
+ 
   redirect '/'
 end
